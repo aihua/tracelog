26bdb1df8b045981090a55cc696084ede30357db
lq <huashiyiqike@qq.com>
2016-10-31 22:57:12 +0800

    more options

--- src/index.ts
+++ src/index.ts
@@ -1,105 +1,148 @@
-function tracelog(sth, expect) { // trace is enabled if expect is not undefined
-    if (expect != null) {
+/// <reference path="../node_modules/@types/colors/index.d.ts" />
+import * as colors  from 'colors';
+namespace tracelogNamespace {
+    function extract(str) {
         try {
-            if (!compare2Objects(sth, expect)) {
-                throw new Error(sth);
-            } else {
-                return true;
-            }
+            str = str.match(/at ([^ ]+) \((.*)\)/);
+            let list = str[2].split('/');
+            return str[1] + '(' + list[list.length - 1] + ')';
         } catch (e) {
-            console.log(e.stack);
-            return false;
+            return e.stack;
         }
-    } else {
-        console.log(sth);
     }
-}
-export default tracelog
-function compare2Objects(x, y) {
-    var p;
-
-    // remember that NaN === NaN returns false
-    // and isNaN(undefined) returns true
-    if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
-        return true;
-    }
-
-    // Compare primitives and functions.     
-    // Check if both arguments link to the same object.
-    // Especially useful on the step where we compare prototypes
-    if (x === y) {
-        return true;
-    }
-
-    // console.log(x, y)
-    // Works in case when functions are created in constructor.
-    // Comparing dates is a common scenario. Another built-ins?
-    // We can even handle functions passed across iframes
-    if ((typeof x === 'function' && typeof y === 'function') ||
-        (x instanceof Date && y instanceof Date) ||
-        (x instanceof RegExp && y instanceof RegExp) ||
-        (x instanceof String && y instanceof String) ||
-        (x instanceof Number && y instanceof Number) ||
-        (typeof (x) !== 'object' && typeof (x) === typeof (y))) {
-        return true;
-    }
-    if(x instanceof Array && y instanceof Array){
-        if(x.length === 0 && y.length > 0){
-            console.warn('array content not specified', x, y)
-            return true;
-        }else{
-            for(var i = 0; i < Math.min(x.length, y.length); i++){
-                if(!compare2Objects(x[i],y[i])){
+    // @ depth: trace depth
+    // @ color: console log color
+    // @ fullprint: trace full information
+    // @ disable: disable log
+    // @ callback(logstr): for outer logger, write to file etc
+    export function tracelog(opt = { depth: 3, color: 'yellow', fullprint: false, disable: false, callback: undefined }) {
+        // @ sth real data
+        // @ expect expected form
+        // @ tmpopt local settings, can override opt
+        return function (sth, expect, tmpopt) { // trace is enabled if expect is not undefined
+            if (opt['disable']) { return; }
+            let color = (str) => {
+                return colors ? colors[opt['color']](str) : str;
+            }
+            let options = (attr) => {
+                return (tmpopt && attr in tmpopt) ? tmpopt[attr] : opt[attr];
+            }
+            if (expect == null) { return; }
+            try {
+                if (!compare2Objects(sth, expect)) {
+                    throw new Error(sth);
+                } else {
+                    return true;
+                }
+            } catch (e) {
+                if (options('fullprint')) {
+                    console.log(JSON.stringify(sth), 'expected:', JSON.stringify(expect), color(e.stack));
+                    if(options('callback') != null){
+                        options('callback')(e.stack);
+                    }
                     return false;
                 }
+                let errStr = e.stack.split('\n').slice(1);
+                let arr = errStr.slice(1, options('depth'));
+                console.log(JSON.stringify(sth), 'expected:', JSON.stringify(expect), color(arr.map(extract).join(' << ')));
+                if(options('callback') != null){
+                    options('callback')([JSON.stringify(sth), 'expected:', JSON.stringify(expect), arr.map(extract).join(' << ')].join(' '));
+                }
+                return false;
             }
         }
+    }
+
+
+
+    function compare2Objects(x, y) {
+        var p;
+
+        // remember that NaN === NaN returns false
+        // and isNaN(undefined) returns true
+        if (isNaN(x) && isNaN(y) && typeof x === 'number' && typeof y === 'number') {
+            return true;
+        }
+
+        // Compare primitives and functions.     
+        // Check if both arguments link to the same object.
+        // Especially useful on the step where we compare prototypes
+        if (x === y) {
+            return true;
+        }
+
+        // console.log(x, y)
+        // Works in case when functions are created in constructor.
+        // Comparing dates is a common scenario. Another built-ins?
+        // We can even handle functions passed across iframes
+        if ((typeof x === 'function' && typeof y === 'function') ||
+            (x instanceof Date && y instanceof Date) ||
+            (x instanceof RegExp && y instanceof RegExp) ||
+            (x instanceof String && y instanceof String) ||
+            (x instanceof Number && y instanceof Number) ||
+            (typeof (x) !== 'object' && typeof (x) === typeof (y))) {
+            return true;
+        }
+        if (x instanceof Array && y instanceof Array) {
+            if (x.length === 0 && y.length > 0) {
+                console.warn('array content not specified', x, y)
+                return true;
+            } else {
+                for (var i = 0; i < Math.min(x.length, y.length); i++) {
+                    if (!compare2Objects(x[i], y[i])) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        // At last checking prototypes as good as we can
+        if (!(x instanceof Object && y instanceof Object)) {
+            return false;
+        }
+
+        if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
+            return false;
+        }
+
+        if (x.constructor !== y.constructor) {
+            return false;
+        }
+
+        if (x.prototype !== y.prototype) {
+            return false;
+        }
+
+
+
+        // Quick checking of one object being a subset of another.
+        // todo: cache the structure of arguments[0] for performance
+        for (p in y) {
+            if (!compare2Objects(y.hasOwnProperty(p), x.hasOwnProperty(p))) {
+                return false;
+            }
+            else if (typeof y[p] !== typeof x[p]) {
+                return false;
+            }
+        }
+        for (p in x) {
+            if (!compare2Objects(y.hasOwnProperty(p), x.hasOwnProperty(p))) {
+                return false;
+            }
+            else if (typeof y[p] !== typeof x[p]) {
+                return false;
+            }
+
+
+            if (!compare2Objects(x[p], y[p])) {
+                return false;
+
+            }
+        }
+
         return true;
     }
 
-    // At last checking prototypes as good as we can
-    if (!(x instanceof Object && y instanceof Object)) {
-        return false;
-    }
-
-    if (x.isPrototypeOf(y) || y.isPrototypeOf(x)) {
-        return false;
-    }
-
-    if (x.constructor !== y.constructor) {
-        return false;
-    }
-
-    if (x.prototype !== y.prototype) {
-        return false;
-    }
-
-
-    
-    // Quick checking of one object being a subset of another.
-    // todo: cache the structure of arguments[0] for performance
-    for (p in y) {
-        if (!compare2Objects(y.hasOwnProperty(p), x.hasOwnProperty(p))) {
-            return false;
-        }
-        else if (typeof y[p] !== typeof x[p]) {
-            return false;
-        }
-    }
-    for (p in x) {
-        if (!compare2Objects(y.hasOwnProperty(p), x.hasOwnProperty(p))) {
-            return false;
-        }
-        else if (typeof y[p] !== typeof x[p]) {
-            return false;
-        }
-
-           
-        if (!compare2Objects(x[p], y[p])) {
-            return false;
-           
-        }
-    }
-
-    return true;
 }
+export default tracelogNamespace.tracelog;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,5 @@
-var tracelog = require('../lib/index.js').default;
+var tracelog = require('../lib/index.js').default(); 
+
 var assert = require("chai").assert;
 describe('test simple', function () {
     it('number', function () {
@@ -7,26 +8,27 @@ describe('test simple', function () {
     it('string', function () {
         assert.deepEqual(tracelog('abc', ''), true);
     });
-    it('array1', function(){
-        assert.deepEqual(tracelog([1,2,3],[1,2]), true);
+    it('array1', function () {
+        assert.deepEqual(tracelog([1, 2, 3], [1, 2]), true);
     });
-    it('array2', function(){
-        assert.deepEqual(tracelog([1,2,3],[]), true);
+    it('array2', function () {
+        assert.deepEqual(tracelog([1, 2, 3], []), true);
     });
-    it('array3', function(){
-        assert.deepEqual(tracelog([],[]), true);
+    it('array3', function () {
+        assert.deepEqual(tracelog([], []), true);
     });
-    it('array4', function(){
-        assert.deepEqual(tracelog([],[1]), true);
+    it('array4', function () {
+        assert.deepEqual(tracelog([], [1]), true);
     });
-    it('array5', function(){
-        assert.deepEqual(tracelog(['fd'],[1]), false);
+    it('array5', function () {
+        assert.deepEqual(tracelog(['fd'], [1]), false);
     });
-    it('array6', function(){
-        assert.deepEqual(tracelog([{a:1}],[{b:1}]), false);
+    it('array6', function () {
+        assert.deepEqual(tracelog([{ a: 1 }], [{ b: 1 }]), false);
     });
 
-    it('different object', function () {
+
+    it('different object1', function () {
         assert.deepEqual(tracelog(
             {
                 errno: 0,
@@ -34,32 +36,54 @@ describe('test simple', function () {
                 data: [{ a: 1, b: 2, c: '' }]
             },
             {
-                errno:209,
-                errmsg:'not true',
-                data:[[]]
+                errno: 209,
+                errmsg: 'not true',
+                data: [[]]
             }), false);
     });
+    it('different object2', function () {
+        assert.deepEqual(tracelog(
+            { 
+                    '25 (forum_id)': {
+                        forum_id: 258,
+                        rank: 0
+                    } 
+            },
+            {
+                    
+                    '258': { 
+                        forum_id: 258, 
+                        rank: 0 
+                    },
+                
+            }), false);
+    });
+    
     it('similar object', function () {
         assert.deepEqual(tracelog(
             {
                 errno: 0,
                 errmsg: '',
-                data: { a: 1, b: 2, c: '', d:[],e:{
-                    a:1,
-                    b:2,
-                    c: function(){},
-                    d:''
-                } }
+                data: {
+                    a: 1, b: 2, c: '', d: [], e: {
+                        a: 1,
+                        b: 2,
+                        c: function () { },
+                        d: ''
+                    }
+                }
             },
             {
-                errno:209,
-                errmsg:'not true',
-                data:{ a: 13, b: 322, c: 'fsaf',d:[12],e:{
-                    a:12,
-                    b:0,
-                    c:function(){return false;},
-                    d:'323'
-                } }
+                errno: 209,
+                errmsg: 'not true',
+                data: {
+                    a: 13, b: 322, c: 'fsaf', d: [12], e: {
+                        a: 12,
+                        b: 0,
+                        c: function () { return false; },
+                        d: '323'
+                    }
+                }
             }), true);
     });
 })
